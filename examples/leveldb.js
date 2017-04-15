00f34af75b8b1830e2078ed98dcb69971543653c
Crzidea <i@crzidea.com>
2017-04-15 11:35:36 +0800

    Style

--- examples/leveldb.js
+++ examples/leveldb.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 'use strict';
 /**
  * Sample of using leveldb to store fetched torrent info.
@@ -9,15 +10,15 @@ var level = require('level');
 var db = level('./leveldb');
 
 var spider = P2PSpider({
-    nodesMaxSize: 200,
-    maxConnections: 400,
-    timeout: 5000
+  nodesMaxSize: 200,
+  maxConnections: 400,
+  timeout: 5000
 });
 
 spider.ignore(function (infohash, rinfo, callback) {
-    db.get(infohash, function (err, value) {
-        callback(!!err);
-    });
+  db.get(infohash, function (err, value) {
+    callback(!!err);
+  });
 });
 
 spider.listen()
@@ -25,20 +26,20 @@ spider.listen()
 setInterval(() => spider.fetch(), 1000)
 
 spider.on('metadata', function (metadata) {
-    var data = {};
-    data.magnet = metadata.magnet;
-    data.name = metadata.info.name ? metadata.info.name.toString() : '';
-    data.fetchedAt = new Date().getTime();
-    db.put(metadata.infohash, JSON.stringify(data), function (err) {
-        if(!err) {
-            console.log(data.name);
-        }
-    });
+  var data = {};
+  data.magnet = metadata.magnet;
+  data.name = metadata.info.name ? metadata.info.name.toString() : '';
+  data.fetchedAt = new Date().getTime();
+  db.put(metadata.infohash, JSON.stringify(data), function (err) {
+    if (!err) {
+      console.log(data.name);
+    }
+  });
 });
 
-process.on('SIGINT', function() {
-    db.close(function(err) {
-        console.log("DB closed!");
-        process.exit();
-    });
+process.on('SIGINT', function () {
+  db.close(function (err) {
+    console.log("DB closed!");
+    process.exit();
+  });
 });
