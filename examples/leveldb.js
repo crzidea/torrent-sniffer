77b91168ec4efe2820c32edfd911b167062e28a7
Crzidea <i@crzidea.com>
2017-04-16 17:53:40 +0800

    Refactoring

--- examples/leveldb.js
+++ examples/leveldb.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
-
-'use strict';
 /**
- * Sample of using leveldb to store fetched torrent info.
+ * Sample of using leveldb to store sniffed torrent info.
  */
-var Spider = require('../lib');
-var level = require('level');
+const Spider = require('../lib');
+const level = require('level');
+const path = require('path')
 
-var db = level('./leveldb');
+const db = level('./leveldb');
 
-var spider = new Spider({
+const spider = new Spider({
   nodesMaxSize: 200,
   maxConnections: 100,
   timeout: 5000
@@ -25,15 +24,29 @@ spider.dht.on('node', () => {
   //console.log(`${spider.dht.nodes.count()} nodes found`);
 })
 
+function lowerCaseExtname(_path) {
+  return path.extname(_path).toLowerCase()
+}
+
 spider.on('metadata', function (metadata) {
-  var data = {};
+  const data = {};
   data.magnet = metadata.magnet;
   data.name = metadata.info.name ? metadata.info.name.toString() : '';
-  data.fetchedAt = new Date().getTime();
+  if (metadata.info.files) {
+    const extnames = new Set
+    metadata.info.files.reduce((extnames, file) => {
+      const extname = lowerCaseExtname(file.path.toString())
+      return extnames.add(extname)
+    }, extnames)
+    data.extnames = [...extnames].join()
+  } else {
+    data.extnames = lowerCaseExtname(data.name)
+  }
   db.put(metadata.infohash, JSON.stringify(data), function (err) {
     if (!err) {
       console.log(data.name);
     }
+    console.log(`holding ${spider.dht.nodes.count()} nodes`);
   });
 });
 
